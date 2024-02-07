57b7ee6343143c41a8e7a3935a0610b15e9a67aa
cesarecicolin <cece.caster@gmail.com>
2024-02-08 00:50:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+<h1>HELLO</h1>
+</template>
+
+<style lang="scss">
+@use"./style/general.scss" as *;
+</style>
